ce70a7a48b095d8727a0947c73543d08265cfc72
Andrei_Gordeev <Andrei_Gordeev@epam.com>
2019-12-17 11:06:51 +0300

    server and client rough implementation

--- client/src/main/java/com/epam/Main.java
+++ client/src/main/java/com/epam/Main.java
@@ -2,9 +2,40 @@ package com.epam;
 
 import org.apache.log4j.Logger;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Main {
     private static final Logger log = Logger.getLogger(Main.class);
+
+    private static Socket clientSocket;
+    private static BufferedReader reader;
+    private static BufferedReader in;
+    private static BufferedWriter out;
     public static void main( String[] args ){
-        log.info("yo bich");
+        try{
+            try{
+                clientSocket = new Socket("localhost", 4470);
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                log.info("Type your message");
+                String line = reader.readLine();
+                out.write(line + '\n');
+                out.flush();
+                String serverLine = in.readLine();
+                log.info(serverLine);
+            }
+            finally {
+                log.info("close client");
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+        }
+        catch (IOException err){
+            log.error(err);
+        }
     }
 }
