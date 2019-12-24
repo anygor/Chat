e2c21b5a97fafa1788ebfc9c3b65a148e281f7e8
Andrei_Gordeev <Andrei_Gordeev@epam.com>
2019-12-24 09:01:53 +0300

    server constructor

--- server/src/main/java/com/epam/Server.java
+++ server/src/main/java/com/epam/Server.java
@@ -11,37 +11,46 @@ import java.util.Properties;
 public class Server {
     private static final Logger log = Logger.getLogger(Server.class);
     public static History history;
-    static LinkedList<ServerThread> serverList = new LinkedList<>();
+    static LinkedList<ServerThread> serverList;
+    private ServerSocket server;
+    private int port;
+    private int timeout;
 
-    public void initiate() {
-        log.info("Server initiated");
+    public Server(){
         try {
             FileInputStream fileInputStream = new FileInputStream("src/main/resources/config.properties");
             Properties property = new Properties();
             property.load(fileInputStream);
-            int port = Integer.parseInt(property.getProperty("db.port"));
-            ServerSocket server = new ServerSocket(port);
+            serverList = new LinkedList<>();
+            port = Integer.parseInt(property.getProperty("db.port"));
+            timeout = Integer.parseInt(property.getProperty("db.timeout"));
+            server = new ServerSocket(port);
             history = new History();
+        }
+        catch(IOException e){
+            log.error("Constructor IO exception");
+        }
+    }
+
+    public void initiate() {
+        log.info("Server initiated");
+        try {
             do {
                 try {
-                    server.setSoTimeout(Integer.parseInt(property.getProperty("db.timeout")));
+                    server.setSoTimeout(timeout);
                     Socket socket = server.accept();
                     serverList.add(new ServerThread(socket));
-                }
-                catch(SocketTimeoutException e){
-                    if(serverList.isEmpty()){
+                } catch (SocketTimeoutException e) {
+                    if (serverList.isEmpty()) {
                         log.info("Empty server timeout exception");
                         History.fileHistoryOutput();
                         server.close();
                         break;
                     }
                 }
-            }while(true);
+            } while (true);
         }
-        catch(FileNotFoundException err) {
-            log.error("config.properties file not found");
-        }
-        catch (IOException err) {
+        catch(IOException err){
             log.error(err);
         }
     }
